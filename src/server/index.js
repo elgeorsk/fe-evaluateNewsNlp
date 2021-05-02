8e5ee58dad31db78604a4fea15b9135909f3ec88
ELGEOR-PC\elgeor <elgeor.sk@gmail.com>
2021-05-02 12:36:46 +0200

    Add dotenv, aylienapi Change port to 8082

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,37 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+let path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-const app = express()
+// Require the Aylien npm package
+let aylien = require("aylien_textapi");
 
-app.use(express.static('dist'))
+// This will allow us to use environment variables we set in a new file
+const dotenv = require('dotenv');
 
-console.log(__dirname)
+dotenv.config();
+
+let aylienapi = new aylien({
+   application_id: process.env.AYLIEN_API_ID,
+   application_key: process.env.AYLIEN_API_KEY
+});
+console.log(`Your API key is ${process.env.AYLIEN_API_KEY}`);
+
+const app = express();
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
-})
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8082, function () {
+    console.log('Example app listening on port 8082!')
+});
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
