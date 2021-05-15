6fa79ba90a3a73aa26208e753fb158b06145a7df
ELGEOR-PC\elgeor <elgeor.sk@gmail.com>
2021-05-15 10:28:16 +0200

    Add jtest with a tru/false test

--- src/__test__/testCheckEnterValue.spec.js
+++ src/__test__/testCheckEnterValue.spec.js
@@ -1,7 +1,36 @@
 // Import the js file to test
-import { checkEnterValue } from '../client/assets/js/checkEnterValue';
+import { checkEnterValue } from '../client/js/checkEnterValue';
 
 describe('Testing the entered value', () => {
-    test('Testing the checkEnterValue() function', () => {
-           expect(checkEnterValue).toBeDefined();
-})});
+    test('Testing valid text',() =>{
+        global.alert = jest.fn();
+        // No error message
+        expect(global.alert).toHaveBeenCalledTimes(0);
+        // Invalid value entered
+        expect(checkEnterValue('Main dishes were quite good, but desserts were too sweet for me.')).toBe('Main dishes were quite good, but desserts were too sweet for me.');
+        // No error message
+        expect(global.alert).toHaveBeenCalledTimes(0);
+    });
+
+    test('Testing invalid text',() =>{
+        // about alert message handling https://stackoverflow.com/questions/55933105/how-to-mock-or-assert-whether-window-alert-has-fired-in-react-jest-with-typesc
+
+        global.alert = jest.fn();
+        // No error message
+        expect(global.alert).toHaveBeenCalledTimes(0);
+        // Invalid value entered
+        expect(checkEnterValue('<h1>Hell there</h1>')).toBe('error');
+        // Alert message appeared
+        expect(global.alert).toHaveBeenCalledTimes(1);
+    });
+
+    test('Double check - Validation of alert message - Test should fail',() =>{
+        global.alert = jest.fn();
+        // No error message
+        expect(global.alert).toHaveBeenCalledTimes(0);
+        // Invalid value entered
+        expect(checkEnterValue('<h1>Hell there</h1>')).toBe('error');
+        // No error message
+        expect(global.alert).toHaveBeenCalledTimes(0);
+    });
+});
